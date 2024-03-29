0014e42a966288e8f36187360092d94524e624a9
ben-jav <a.javadi@web.de>
2024-03-29 23:22:02 +0100

    show answer

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { mockQuestions } from '../mockData';
 import { QuestionType } from '../enums';
+import { Question } from '../Question';
 
 @Component({
   selector: 'ne4-learn',
@@ -10,10 +11,27 @@ import { QuestionType } from '../enums';
 export class LearnComponent {
   questions = mockQuestions;
   questionType = QuestionType;
+  showingAnswerIndex: number | null = null;
+  answer: string = '';
+
+  singleView: boolean = false;
+  currentIndex: number = 0;
 
   constructor() { }
 
   ngOnInt() : void {
+  }
 
+  get currentQuestion() : Question {
+    return this.questions[this.currentIndex];
+  }
+
+  showAnswer(i: number) : void {
+    this.showingAnswerIndex = i;
+    if (this.questions[i].type === this.questionType.FillIn) {
+      if (typeof(this.questions[i].answer) === 'string') {
+        this.answer = this.questions[i].answer as string;
+      }
+    }
   }
 }
